745a3de714036a471e728c7fb86d922b17071a57
kamakshimahaj <kamakshimhjn@gmail.com>
2026-03-09 16:43:53 +0530

    changes done

--- js/supabase.js
+++ js/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
 export const supabase = createClient(
   "https://vwvskyxthnwscelhxvma.supabase.co",
-  "sb_secret_gwG_AHQ2aA-orOY_yN0UBg_FEA8kmck"
+  "sb_publishable_qcwlJ1XOB6bgxv0uvAIOVQ_8U5edIux"
 );
