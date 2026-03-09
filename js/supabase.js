9f3100b6a93d1821d1c46da3d0ee8dd2534410d7
kamakshimahaj <kamakshimhjn@gmail.com>
2026-03-09 16:56:15 +0530

    removed secrets

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 
-export const supabase = createClient(
-  "https://vwvskyxthnwscelhxvma.supabase.co",
-  "sb_publishable_qcwlJ1XOB6bgxv0uvAIOVQ_8U5edIux"
-);
+// export const supabase = createClient(
+//   "https://vwvskyxthnwscelhxvma.supabase.co",
+//   "sb_publishable_qcwlJ1XOB6bgxv0uvAIOVQ_8U5edIux"
+// );
